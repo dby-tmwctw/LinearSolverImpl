11c7799fbf8e82160b2aa65e162031fe9ecfb765
Boyuan Deng <bd31@wavefront.ece.rice.edu>
2020-07-05 16:22:50 -0500

    Wrapped process in functions

--- CUDAImpls/FISTA.cu
+++ CUDAImpls/FISTA.cu
@@ -63,6 +63,67 @@ void shrink(int n, float *x, float l)
     }
 }
 
+float step_size(cublasHandle_t handle, cusolverDnHandle_t cusolverH, int m, int n, float *A)
+{
+    // A should be an mxn matrix;
+    float *d_work;
+    int lwork = 0;
+    int *devInfo;
+    float *eigen;
+    float *AT;
+    float *transpose;
+    cudaMallocManaged(&AT, m*n*sizeof(float));
+    cudaMallocManaged(&transpose, n*n*sizeof(float));
+    cudaMallocManaged(&eigen, n*sizeof(float));
+    cudaMallocManaged(&devInfo, sizeof(int));
+    cudaDeviceSynchronize();
+    cudaMemset(&transpose, 0, n*n*sizeof(float));
+    cudaDeviceSynchronize();
+    copy_array<<<dim3((m+15) / 16, (n+15) / 16), dim3(16, 16)>>>(m, n, A, AT);
+    cudaDeviceSynchronize();
+    float a = 1.0f;
+    float c = 0.0f;
+    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, m, &a, AT, m, A, m, &c, transpose, n);
+    cudaDeviceSynchronize();
+    cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_NOVECTOR;
+    cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
+    cusolverDnSsyevd_bufferSize(cusolverH, jobz, uplo, n, transpose, n, eigen, &lwork);
+    cudaMalloc((void**)&d_work, sizeof(float)*lwork);
+    cusolverDnSsyevd(cusolverH, jobz, uplo, n, transpose, n, eigen, d_work, lwork, devInfo);
+    cudaDeviceSynchronize();
+    float step = 1 / (2 * eigen[n-1]);
+    cudaFree(eigen);
+    cudaFree(devInfo);
+    cudaFree(d_work);
+    return step;
+}
+
+void ista(cublasHandle_t handle, cusolverDnHandle_t cusolverH, int m, int n, float *A, float *x, float *b, float *x_est, int iters, float l)
+{
+    float *result, *temp;
+    cudaMallocManaged(&result, m*sizeof(float));
+    cudaMallocManaged(&temp, n*sizeof(float));
+    cudaDeviceSynchronize();
+    float step = step_size(handle, cusolverH, m, n, A);
+    float a = 1.0f;
+    float c = 0.0f;
+    for (int k = 0; k < iters; k++)
+    {
+        cublasSgemv(handle, CUBLAS_OP_N, m, n, &a, A, m, x_est, 1, &c, result, 1);
+        cudaDeviceSynchronize();
+        vector_minus<<<1, m>>>(m, result, b);
+        cudaDeviceSynchronize();
+        cublasSgemv(handle, CUBLAS_OP_T, m, n, &a, A, m, result, 1, &c, temp, 1);
+        cudaDeviceSynchronize();
+        vector_multiply<<<1, n>>>(n, 2*step, temp);
+        cudaDeviceSynchronize();
+        vector_minus<<<1, n>>>(n, x_est, temp);
+        cudaDeviceSynchronize();
+        shrink<<<1, n>>>(n, x_est, l * step);
+        cudaDeviceSynchronize();
+    }
+}
+
 int main(void)
 {
     cublasHandle_t handle;
@@ -73,27 +134,17 @@ int main(void)
     int i, j;
     unsigned long long seed = 123456;
     // A is mxn matrix
-    float *A, *AT;
-    float *transpose;
-    float *eigen;
+    float *A;
     // x is R^n, b is R^m
     float *x, *b;
     int m = 32;
     int n = 16;
-    int *devInfo;
-    float *d_work;
-    int lwork = 0;
     int info_gpu = 0;
     cudaMallocManaged(&A, m*n*sizeof(float));
-    cudaMallocManaged(&AT, m*n*sizeof(float));
     cudaMallocManaged(&x, n*sizeof(float));
     cudaMallocManaged(&b, m*sizeof(float));
-    cudaMallocManaged(&transpose, n*n*sizeof(float));
-    cudaMallocManaged(&eigen, n*sizeof(float));
-    cudaMallocManaged(&devInfo, sizeof(int));
     cudaDeviceSynchronize();
     cudaMemset(&b, 0, m*sizeof(float));
-    cudaMemset(&transpose, 0, n*n*sizeof(float));
     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MTGP32);
     curandSetPseudoRandomGeneratorSeed(gen, seed);
     cudaDeviceSynchronize();
@@ -101,72 +152,32 @@ int main(void)
     curandGenerateUniform(gen, x, n*sizeof(float));
     cudaDeviceSynchronize();
     // cudaMemcpy(&AT, &A, m*n*sizeof(float), cudaMemcpyDefault);
-    copy_array<<<dim3((m+15) / 16, (n+15) / 16), dim3(16, 16)>>>(m, n, A, AT);
-    cudaDeviceSynchronize();
     for (i = 0; i < n; i++)
     {
         printf("%f\n", x[i]);
     }
     printf("-------------------------\n");
-    stat = cublasCreate(&handle);
     float a = 1.0f;
     float c = 0.0f;
+    stat = cublasCreate(&handle);
     stat = cublasSgemv(handle, CUBLAS_OP_N, m, n, &a, A, m, x, 1, &c, b, 1);
     cudaDeviceSynchronize();
-    // cuBLAS's explanation kind of misleading here. The leading dimension is before any operation
-    stat = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, m, &a, AT, m, A, m, &c, transpose, n);
-    cudaDeviceSynchronize();
     cusolver_status = cusolverDnCreate(&cusolverH);
-    cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_NOVECTOR;
-    cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
-    cusolver_status = cusolverDnSsyevd_bufferSize(cusolverH, jobz, uplo, n, transpose, n, eigen, &lwork);
-    cudaMalloc((void**)&d_work, sizeof(float)*lwork);
-    cusolver_status = cusolverDnSsyevd(cusolverH, jobz, uplo, n, transpose, n, eigen, d_work, lwork, devInfo);
-    cudaDeviceSynchronize();
-    // for (i = 0; i < n; i++)
-    // {
-    //     printf("%f ", eigen[i]);
-    // }
-    // printf("\n");
-    int iters = 1000;
-    float step = 1 / (2 * eigen[n-1]);
+    int iters = 8000;
     float l = 0.00001;
-    float *x_est, *result, *temp;
+    float *x_est;
     cudaMallocManaged(&x_est, n*sizeof(float));
-    cudaMallocManaged(&result, m*sizeof(float));
-    cudaMallocManaged(&temp, n*sizeof(float));
     cudaDeviceSynchronize();
     cudaMemset(&x_est, 0, n*sizeof(float));
-    for (int k = 0; k < iters; k++)
-    {
-        stat = cublasSgemv(handle, CUBLAS_OP_N, m, n, &a, A, m, x_est, 1, &c, result, 1);
-        cudaDeviceSynchronize();
-        vector_minus<<<1, m>>>(m, result, b);
-        cudaDeviceSynchronize();
-        stat = cublasSgemv(handle, CUBLAS_OP_T, m, n, &a, A, m, result, 1, &c, temp, 1);
-        cudaDeviceSynchronize();
-        vector_multiply<<<1, n>>>(n, 2*step, temp);
-        cudaDeviceSynchronize();
-        vector_minus<<<1, n>>>(n, x_est, temp);
-        cudaDeviceSynchronize();
-        shrink<<<1, n>>>(n, x_est, l * step);
-        cudaDeviceSynchronize();
-    }
+    ista(handle, cusolverH, m, n, A, x, b, x_est, iters, l);
     for (j = 0; j < n; j++)
     {
         printf("%f\n", x_est[j]);
     }
     cudaFree(A);
-    cudaFree(AT);
-    cudaFree(transpose);
-    cudaFree(eigen);
-    cudaFree(devInfo);
-    cudaFree(d_work);
     cudaFree(x);
     cudaFree(b);
     cudaFree(x_est);
-    cudaFree(result);
-    cudaFree(temp);
     cublasDestroy(handle);
     curandDestroyGenerator(gen);
 }
